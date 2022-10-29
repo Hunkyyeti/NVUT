5d0b5647b7246edfb9df60eb5d34adbfc50e3c63
Hunkyyeti <67403175+Hunkyyeti@users.noreply.github.com>
2022-10-29 18:42:12 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,3 @@
-#------To-Do------#
-#Fragment code into individual files | Done
-#Make keyboard script | needs shift button and keytracer
-#Add file picker | not started
-#Add start up animation | not started
-#Add apps menu | not started
-#Bad Apple | not started
-#Snake | not started
-#
-#
-#
-#
-# Display Image & text on I2C driven ssd1306 OLED display 
 import fileManager
-import keyboard
 
-keyboard.keyboard()
 fileManager.fileManager()
